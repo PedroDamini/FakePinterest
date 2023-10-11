627274926d259fcddca98346b9018f38cd2692cd
unknown <pedrodelemosdamini@gmail.com>
2023-10-11 17:34:23 -0300

    criação dos templates iniciais

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<p>Hello World!</p>"
+    return render_template("homepage.html")
+
+@app.route("/perfil")
+def perfil():
+    return "Perfil"
 
 if __name__ == "__main__":
     app.run(debug=True)
