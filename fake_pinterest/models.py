bed77736f509bf4aa376b7d1751453daeb1d27b8
unknown <pedrodelemosdamini@gmail.com>
2023-10-19 17:47:16 -0300

    ajuste models

--- fake_pinterest/models.py
+++ fake_pinterest/models.py
@@ -2,15 +2,15 @@ from fake_pinterest import database
 from datetime import datetime
 
 class Usuario(database.Model):
-    id = database.Collumn(database.Integer, primary_key=True)
-    username = database.Collumn(database.String, nullable=False, unique=True)
-    email = database.Collumn(database.String, nullable=False, unique=True)
-    senha = database.Collumn(database.String, nullable=False)
-    fotos = database.relationship()
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String, nullable=False, unique=True)
+    email = database.Column(database.String, nullable=False, unique=True)
+    senha = database.Column(database.String, nullable=False)
+    fotos = database.relationship("Foto", backref="usuario", lazy=True)
 
 
 class Foto(database.Model):
-    id = database.Collumn(database.Integer, primary_key=True)
-    imagem = database.Collumn(database.String, default="default.png")
-    data_criacao = database.Collumn(database.DateTime, nullable=False, default=datetime.utcnow())
-    id_usuario = database.Collumn()
+    id = database.Column(database.Integer, primary_key=True)
+    imagem = database.Column(database.String, default="default.png")
+    data_criacao = database.Column(database.DateTime, nullable=False, default=datetime.utcnow())
+    id_usuario = database.Column(database.Integer, database.ForeignKey("usuario.id"), nullable=False)
