6cf77015c78cb6bd028a05aa8d8833aa090735de
unknown <pedrodelemosdamini@gmail.com>
2023-10-23 15:06:30 -0300

    ajustando sistema de login

--- fake_pinterest/routes.py
+++ fake_pinterest/routes.py
@@ -1,10 +1,12 @@
 from flask import render_template, url_for
 from fake_pinterest import app
+from flask_login import login_required
 
 @app.route("/")
 def home():
     return render_template("homepage.html")
 
 @app.route("/perfil/<usuario>")
+@login_required
 def perfil(usuario):
     return render_template("perfil.html", usuario=usuario)
