22b56a676a2fe00602d75d2a514bc26baeb38db7
unknown <pedrodelemosdamini@gmail.com>
2023-10-23 14:32:10 -0300

    criando sistema de login

--- fake_pinterest/__init__.py
+++ fake_pinterest/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SECRET_KEY"] = "dee4762687b9c9863f791a8536cf4580154c6a53"
 
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "homepage"
 
 from fake_pinterest import routes
