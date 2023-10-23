74f40a0221c169d87c2dda3e2b6f59bd4331ea43
unknown <pedrodelemosdamini@gmail.com>
2023-10-23 16:37:23 -0300

    ajustando formularios no front

--- fake_pinterest/routes.py
+++ fake_pinterest/routes.py
@@ -1,10 +1,17 @@
 from flask import render_template, url_for
 from fake_pinterest import app
 from flask_login import login_required
+from fake_pinterest.forms import FormCriarConta, FormLogin
 
 @app.route("/")
-def home():
-    return render_template("homepage.html")
+def homepage():
+    formlogin = FormLogin()
+    return render_template("homepage.html", form=formlogin)
+
+@app.route("/criaconta")
+def criar_conta():
+    formcriarconta = FormCriarConta
+    return render_template("criar_conta.html", form=formcriarconta)
 
 @app.route("/perfil/<usuario>")
 @login_required
